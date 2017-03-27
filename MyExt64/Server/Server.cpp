b8c10497b4b5faf98d5f72d4a7f51826bc51305c
Emca <emca@eressea.cz>
2017-03-27 16:56:05 +0100

    hide log server/petition server warning

--- MyExt64/Server/Server.cpp
+++ MyExt64/Server/Server.cpp
@@ -225,6 +225,8 @@ void Server::HideWarnings()
 	NOPMemory(0x5EA1E2, 5);
 	NOPMemory(0x6D323A, 5);
 	NOPMemory(0x92DFEA, 5);
+	NOPMemory(0x6BAC72, 5);
+	NOPMemory(0x7B9195, 5);
 }
 
 void Server::OnLoadEnd(UINT64 classBase)
